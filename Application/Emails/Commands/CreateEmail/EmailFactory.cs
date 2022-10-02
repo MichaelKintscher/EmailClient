b1da9e319dd1c9c5ebb765e3fe888affcaa45d29
Michael Kintscher <michael@kintscher.dev>
2022-10-01 19:45:09 -0700

    Added email factory

---
+++ Application/Emails/Commands/CreateEmail/EmailFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Emails;
+
+namespace Application.Emails.Commands.CreateEmail
+{
+    /// <summary>
+    /// Factory class for creating emails.
+    /// </summary>
+    public class EmailFactory
+    {
+        /// <summary>
+        /// Creates a new email with the given subject and body.
+        /// </summary>
+        /// <param name="subject">The subject to give the email.</param>
+        /// <param name="body">The body to give the email.</param>
+        /// <returns>The created email.</returns>
+        public Email Create(string subject, string body)
+        {
+            // Create a new instance of the email class.
+            Email email = new Email
+            {
+                // Assign the subject and body properties from the input parameters.
+                Subject = subject,
+                Body = body
+            };
+
+            // Return the new email instance.
+            return email;
+        }
+    }
+}
--- Domain/Emails/Email.cs
+++ Domain/Emails/Email.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Email
+namespace Domain.Emails
 {
     /// <summary>
     /// Model for an email message.
@@ -14,11 +14,20 @@ namespace Domain.Email
         /// <summary>
         /// The subject of the email.
         /// </summary>
-        public string? Subject { get; set; }
+        public string Subject { get; set; }
 
         /// <summary>
         /// The body of the email.
         /// </summary>
-        public string? Body { get; set; }
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Default constructor - creates an email with an empty string subject and body.
+        /// </summary>
+        public Email()
+        {
+            this.Subject = "";
+            this.Body = "";
+        }
     }
 }
