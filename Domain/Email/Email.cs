3b397126c7cd100373e86e348df1baea5ff5961e
Michael Kintscher <michael@kintscher.dev>
2022-10-01 19:08:25 -0700

    Created email model

---
+++ Domain/Email/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Email
+{
+    /// <summary>
+    /// Model for an email message.
+    /// </summary>
+    public class Email
+    {
+        /// <summary>
+        /// The subject of the email.
+        /// </summary>
+        public string? Subject { get; set; }
+
+        /// <summary>
+        /// The body of the email.
+        /// </summary>
+        public string? Body { get; set; }
+    }
+}
